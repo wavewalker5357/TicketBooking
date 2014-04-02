9878ec67f18756efc12d7824afd5fccd9e7bde78
rozklad <jan.rozklad@gmail.com>
2014-04-02 11:19:11 +0200

    autoloading

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php include_once('autoload.php'); ?>
+<?php include_once(__DIR__ . '/vendor/autoload.php'); ?>
 <!DOCTYPE html>
 <!--
 To change this license header, choose License Headers in Project Properties.
